9d88a1ee04375112fbe93b87bab75fecd7a1e1af
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-04-27 15:25:46 +0000

    Ajout de la classe Material

---
+++ trunk/Material.hpp
@@ -0,0 +1,63 @@
+//
+// Material.hpp for raytracer in /home/michar_l//Raytracer/raytracer-42
+// 
+// Made by loick michard
+// Login   <michar_l@epitech.net>
+// 
+// Started on  Wed Apr 27 15:50:09 2011 loick michard
+// Last update Wed Apr 27 17:24:30 2011 loick michard
+//
+
+#ifndef _MATERIAL_HPP_
+#define _MATERIAL_HPP_
+
+#include <string>
+
+using namespace std;
+
+class QImage;
+
+class Material
+{
+public:
+  Material();
+  Material(const string& name);
+  Material(const Texture& texture);
+  ~Material();
+
+  void		setColor(const Color& color);
+  void		setTexture(const Texture& texture);
+  void		setSpecularCoeff(double specularCoeff);
+  void		setSpecularPow(double specularPow);
+  void		setReflectionCoeff(double reflectionCoeff);
+  void		setTransmissionCoeff(double transmissionCoeff);
+  void		setRefractionIndex(double refractionIndex);
+  void		setName(string name);
+  void		setHeightmap(const Texture& image);
+
+  double	getSpecularCoeff() const;
+  double	getSpecularPow() const;
+  double	getReflectionCoeff() const;
+  double	getTransmissionCoeff() const;
+  double	getRefractionIndex() const;
+  string	getName() const;
+
+  Color&       	getColor(double x, double y) const;  
+  Vector&	getDeformatedNormal(double x, double y,
+				    const Vector& normal) const;
+
+private:
+  string	_name;
+  Color		_color;
+  Texture	_texture;
+  bool		_isTextured;
+  double	_specularCoeff;
+  double	_specularPow;
+  double	_reflectionCoeff;
+  double	_transmissionCoeff;
+  double	_refractionIndex;
+  Texture	_heightmap;
+  bool		_hasBumpMap;
+};
+
+#endif
