eeb3416ae61cb9a8f4e3666c2b42760321d64c18
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-04-27 17:52:12 +0000

    Ca compile !!! (Pour de vrai)

--- trunk/Scene.cpp
+++ trunk/Scene.cpp
@@ -5,7 +5,7 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Wed Apr 27 18:24:15 2011 loick michard
-// Last update Wed Apr 27 18:24:51 2011 loick michard
+// Last update Wed Apr 27 19:52:09 2011 loick michard
 //
 
 #include "Scene.hpp"
