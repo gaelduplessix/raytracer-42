cd58cb2c074443ad86fc2f49daeb075e844a560e
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-04-28 12:07:35 +0000

    Classe Vector: done

--- trunk/Vector.cpp
+++ trunk/Vector.cpp
@@ -4,18 +4,167 @@
 // Made by loick michard
 // Login   <michar_l@epitech.net>
 // 
-// Started on  Wed Apr 27 18:51:08 2011 loick michard
-// Last update Wed Apr 27 22:05:04 2011 samuel olivier
+// Started on  Wed Apr 27 22:40:10 2011 loick michard
+// Last update Thu Apr 28 14:05:22 2011 loick michard
 //
 
+#include <cmath>
 #include "Vector.hpp"
 
 Vector::Vector()
 {
+  _x = _y = _z = 0;
+}
 
+Vector::Vector(double x, double y, double z)
+{
+  _x = x;
+  _y = y;
+  _z = z;
 }
 
 Vector::~Vector()
 {
 
 }
+
+double	Vector::getX(void) const
+{
+  return (_x);
+}
+
+double	Vector::getY(void) const
+{
+  return (_y);
+}
+
+double	Vector::getZ(void) const
+{
+  return (_z);
+}
+
+void	Vector::setX(double x)
+{
+  _x = x;
+}
+
+void	Vector::setY(double y)
+{
+  _y = y;
+}
+
+void	Vector::setZ(double z)
+{
+  _z = z;
+}
+
+double	Vector::getNorm(void)
+{
+  return (sqrt(_x * _x + _y * _y + _z * _z));
+}
+
+void	Vector::normalize(void)
+{
+  double norm;
+
+  norm = this->getNorm();
+  _x = _x / norm;
+  _y = _y / norm;
+  _z = _z / norm;
+}
+
+void	Vector::rotateX(double alpha)
+{
+  double yp;
+  double zp;
+
+  yp = cos(alpha) * _y - sin(alpha) * _z;
+  zp = sin(alpha) * _y + cos(alpha) * _z;
+  _y = yp;
+  _z = zp;
+}
+
+void    Vector::rotateY(double alpha)
+{
+  double xp;
+  double zp;
+
+  xp = cos(alpha) * _x + sin(alpha) * _z;
+  zp = - sin(alpha) * _x + cos(alpha) * _z;
+  _x = xp;
+  _z = zp;
+}
+
+void    Vector::rotateZ(double alpha)
+{
+  double xp;
+  double yp;
+
+  xp = cos(alpha) * _x - sin(alpha) * _y;
+  yp = - sin(alpha) * _x + cos(alpha) * _y;
+  _x = xp;
+  _y = yp;
+}
+
+void	Vector::rotate(Vector& rotation)
+{
+  this->rotateX(rotation.getX());
+  this->rotateY(rotation.getY());
+  this->rotateZ(rotation.getZ());
+}
+
+Vector&	Vector::operator+=(const Vector& vector)
+{
+  _x += vector.getX();
+  _y += vector.getY();
+  _z += vector.getZ();
+  return (*this);
+}
+
+Vector&	Vector::operator-=(const Vector& vector)
+{
+  _x -= vector.getX();
+  _y -= vector.getY();
+  _z -= vector.getZ();
+  return (*this);
+}
+
+Vector&	Vector::operator*=(const Vector& vector)
+{
+  double xp;
+  double yp;
+  double zp;
+
+  xp = _y * vector.getZ() - _z * vector.getY();
+  yp = _z * vector.getX() - _x * vector.getZ();
+  zp = _x * vector.getY() - _y * vector.getX();
+  _x = xp;
+  _y = yp;
+  _z = zp;
+  return (*this);
+}
+
+Vector	operator+(const Vector& vector1, const Vector& vector2)
+{
+  Vector vector = vector1;
+
+  vector += vector2;
+  return (vector);
+}
+
+Vector	operator-(const Vector& vector1, const Vector& vector2)
+{
+  Vector vector = vector1;
+
+  vector -= vector2;
+  return (vector);
+}
+
+Vector	operator*(const Vector& vector1, const Vector& vector2)
+{
+  Vector vector = vector1;
+
+  vector *= vector2;
+  return (vector);
+}
+
--- trunk/Vector.hpp
+++ trunk/Vector.hpp
@@ -1,13 +1,12 @@
 //
-// Vector.hpp for raytracer in /home/jochau_g//dev/c/raytracer/raytracer-42
+// Vector.hpp for raytracer in /home/michar_l//Raytracer/raytracer-42
 // 
-// Made by gael jochaud-du-plessix
-// Login   <jochau_g@epitech.net>
+// Made by loick michard
+// Login   <michar_l@epitech.net>
 // 
-// Started on  Tue Apr 26 18:21:04 2011 gael jochaud-du-plessix
-// Last update Wed Apr 27 22:05:12 2011 samuel olivier
+// Started on  Wed Apr 27 22:39:57 2011 loick michard
+// Last update Thu Apr 28 14:01:15 2011 loick michard
 //
-
 
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
@@ -18,21 +17,35 @@ class Vector
 {
 public:
   Vector();
+  Vector(double x, double y, double z);
   ~Vector();
   
-  double	getX() const;
-  double	getY() const;
-  double	getZ() const;
+  double	getX(void) const;
+  double	getY(void) const;
+  double	getZ(void) const;
   void		setX(double value);
   void		setY(double value);
   void		setZ(double value);
-  void		rotate(Rotation& rotation);
-  void		unitarize();
+
+  double	getNorm(void);
+  void		rotate(Vector& rotation);
+  void		normalize(void);
+
+  Vector&       operator+=(const Vector& vector);
+  Vector&       operator-=(const Vector& vector);
+  Vector&       operator*=(const Vector& vector);
 
 private:
+  void		rotateX(double alpha);
+  void		rotateY(double alpha);
+  void		rotateZ(double alpha);
   double	_x;
   double	_y;
   double	_z;
 };
+
+Vector   operator+(const Vector& vector1, const Vector& vector2);
+Vector   operator-(const Vector& vector1, const Vector& vector2);
+Vector   operator*(const Vector& vector1, const Vector& vector2);
 
 #endif
