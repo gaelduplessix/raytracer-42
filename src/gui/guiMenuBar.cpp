936c400d4113dbe650f5797c677448f0e592ddc6
franck.lavisse <franck.lavisse@epitech.eu>
2011-04-29 18:03:42 +0000

    LA ca marche

---
+++ src/gui/guiMenuBar.cpp
@@ -0,0 +1,18 @@
+//
+// guiMenuBar.cpp for  in /home/laviss_f//raytracer-42/gui
+// 
+// Made by franck lavisse
+// Login   <laviss_f@epitech.net>
+// 
+// Started on  Fri Apr 29 19:30:28 2011 franck lavisse
+// Last update Fri Apr 29 19:40:23 2011 franck lavisse
+//
+#include "gui.hpp"
+
+void	Gui::menuBar(void)
+{
+  QMenuBar	*menu = new QMenuBar();
+  QDockWidget	*dock = new QDockWidget();
+  
+  dock->setAllowedAreas
+}
