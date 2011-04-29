117c023c4f68d4ce723937dcc9b1a4aa3c577638
samuel.olivier <samuel.olivier@epitech.eu>
2011-04-29 19:22:03 +0000

    Modif putPixel with class Color

--- src/gui/guiPutpixel.cpp
+++ src/gui/guiPutpixel.cpp
@@ -5,18 +5,20 @@
 // Login   <laviss_f@epitech.net>
 // 
 // Started on  Fri Apr 29 15:39:43 2011 franck lavisse
-// Last update Fri Apr 29 15:40:04 2011 franck lavisse
+// Last update Fri Apr 29 21:21:03 2011 samuel olivier
 //
-#include "gui.hpp"
 
-void	Gui::putPixel(unsigned char color[3], int x, int y)
+#include "gui.hpp"
+#include "../Color.hpp"
+
+void	Gui::putPixel(const Color& color, int x, int y)
 {
   QPolygon	Pixel;
   QGraphicsItem	*Item;
   QColor	Color;
-
-  Color.setRgb((int)color[0], (int)color[1], (int)color[2], 255);
   QPen		PixelPen(Color, 0, Qt::SolidLine);
   QBrush	PixelBrush(Color);
-  Item = _Scene->addRect(x,y,1,1,PixelPen,PixelBrush);
+
+  Color.setRgb(color.getR(), color.getG(), color.getB(), color.getA());
+  Item = _Scene->addRect(x, y, 1, 1, PixelPen, PixelBrush);
 }
