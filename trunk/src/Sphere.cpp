83569312ed3f64c0be9ab2e27ddd07386fed88d3
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-04-30 14:29:55 +0000

    Opti algo raytracing

--- trunk/src/Sphere.cpp
+++ trunk/src/Sphere.cpp
@@ -5,10 +5,11 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Fri Apr 29 10:41:20 2011 loick michard
-// Last update Sat Apr 30 13:50:41 2011 gael jochaud-du-plessix
+// Last update Sat Apr 30 16:28:51 2011 loick michard
 //
 
 #include <vector>
+#include "Raytracer.hpp"
 #include "Sphere.hpp"
 #include "EquationSolver.hpp"
 
@@ -28,32 +29,71 @@ void		Sphere::setRadius(double r)
   _radius = r;
 }
 
-vector<double>	Sphere::intersectWithRay(const Ray& ray) const
+void        Sphere::addIntersectionWithRay(const Ray& ray, vector<struct s_intersected_object>& intersection) const
 {
-  Ray		newRay;
-  double	a;
-  double	b;
-  double	c;
+  Ray           newRay;
+  double        a;
+  double        b;
+  double        c;
 
   newRay = getRayAtSimplePosition(ray);
   a = newRay._vector._x * newRay._vector._x +
     newRay._vector._y * newRay._vector._y +
     newRay._vector._z * newRay._vector._z;
   b = 2 * (newRay._point._x * newRay._vector._x +
-	   newRay._point._y * newRay._vector._y +
-	   newRay._point._z * newRay._vector._z);
+           newRay._point._y * newRay._vector._y +
+           newRay._point._z * newRay._vector._z);
   c = newRay._point._x * newRay._point._x +
     newRay._point._y * newRay._point._y +
     newRay._point._z * newRay._point._z -
     _radius * _radius;
-  return (EquationSolver::solveQuadraticEquation(a, b, c));
+  vector<double> solutions = 
+    EquationSolver::solveQuadraticEquation(a, b, c);
+  for (int i = 0; i++; i < solutions.size())
+    {
+      if (solutions[i] > EPSILON)
+	{
+	  intersection.push_back((t_intersected_object){this, solutions});
+	  break;
+	}
+    } 
+}
+
+void                  Sphere::intersectWithRay(const Ray& ray,
+					       ObjectPrimitive*& primitive, 
+					       double &res) const
+{
+  Ray           newRay;
+  double        a;
+  double        b;
+  double        c;
+
+  newRay = getRayAtSimplePosition(ray);
+  a = newRay._vector._x * newRay._vector._x +
+    newRay._vector._y * newRay._vector._y +
+    newRay._vector._z * newRay._vector._z;
+  b = 2 * (newRay._point._x * newRay._vector._x +
+           newRay._point._y * newRay._vector._y +
+           newRay._point._z * newRay._vector._z);
+  c = newRay._point._x * newRay._point._x +
+    newRay._point._y * newRay._point._y +
+    newRay._point._z * newRay._point._z -
+    _radius * _radius;
+  vector<double> solutions =
+    EquationSolver::solveQuadraticEquation(a, b, c);
+  for (int i = 0; i++; i < solutions.size())
+    {
+      if (solutions[i] > EPSILON && (solutions[i] < res ||  res < 1))
+        {
+	  primitive = (ObjectPrimitive*)this;
+	  res = solutions[i];
+	}
+    }
 }
 
 Vector		Sphere::getNormalVector(const Point& intersectPoint) const
 {
-  Vector	normal = intersectPoint - _absolutePosition;
-
-  return (normal);
+  return (intersectPoint - _absolutePosition);
 }
 
 Vector		Sphere::getReflectedVector(const Point& intersectPoint,
