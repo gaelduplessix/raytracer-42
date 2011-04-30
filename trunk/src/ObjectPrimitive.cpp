3ecaeb92941f87b7b0b33d0a9ba97cd6e95fe4a0
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-04-30 10:09:14 +0000

    OPTI

--- trunk/src/ObjectPrimitive.cpp
+++ trunk/src/ObjectPrimitive.cpp
@@ -5,7 +5,7 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Wed Apr 27 18:55:34 2011 loick michard
-// Last update Sat Apr 30 11:20:02 2011 loick michard
+// Last update Sat Apr 30 12:08:01 2011 loick michard
 //
 
 #include "ObjectPrimitive.hpp"
@@ -71,18 +71,17 @@ void          ObjectPrimitive::setMaterial(const Material& material)
   _material = material;
 }
 
-Ray           ObjectPrimitive::getRayAtSimplePosition(const Ray& ray) const
+inline Ray           ObjectPrimitive::getRayAtSimplePosition(const Ray& ray) const
 {
   Ray	      modified = ray;
   Vector      vector = ray.getVector();
-  Rotation    rotation = _rotation * -1;
   Point       point = ray.getPoint();
 
-  vector.rotate(rotation);
+  vector.rotate(_rotation, 1);
   modified.setVector(vector);
 
   point -= _absolutePosition;
-  point.rotate(rotation);
+  point.rotate(_rotation, 1);
   modified.setPoint(point);
 
   return (modified);
