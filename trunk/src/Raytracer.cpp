7486c1ad2fc86315cedabe4149127e6c3e881350
gael.duplessix@gmail.com <gael.duplessix@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-02 14:18:21 +0000

    Petit probleme de //

--- trunk/src/Raytracer.cpp
+++ trunk/src/Raytracer.cpp
@@ -5,7 +5,7 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Wed Apr 27 18:02:30 2011 loick michard
-   Last update Mon May  2 16:15:45 2011 gael jochaud-du-plessix
+// Last update Mon May  2 16:17:54 2011 gael jochaud-du-plessix
 //
 
 #include <stdio.h>
