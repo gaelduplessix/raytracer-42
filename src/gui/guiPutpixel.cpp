642930bfa450c9332b0c21c227cb0365977b33a8
franck.lavisse <franck.lavisse@epitech.eu>
2011-05-09 22:07:10 +0000

    export png/jpeg/gif/bmp + gestion derreur de mauvais format, format par defaut = png + jochau_g ton rendu est un peu lent :((

--- src/gui/guiPutpixel.cpp
+++ src/gui/guiPutpixel.cpp
@@ -5,21 +5,43 @@
 // Login   <laviss_f@epitech.net>
 // 
 // Started on  Fri Apr 29 15:39:43 2011 franck lavisse
-// Last update Mon May  9 23:10:37 2011 franck lavisse
+// Last update Mon May  9 23:44:06 2011 franck lavisse
 //
 #include <iostream>
+#include <string>
+#include <vector>
 #include <QFileDialog>
 #include "gui.hpp"
 #include "../Color.hpp"
 
 using namespace std;
 
+
+
 void	Gui::saveImage(void)
 {
-  //  QMessageBox::information(this, "Sauvegarde de scene",
+  vector<string> validFormat(4);
+  validFormat[0] = "png";
+  validFormat[1] = "gif";
+  validFormat[2] = "bmp";
+  validFormat[3] = "peg";
+  string format;
   //  _image->save("raytracer.png", "PNG");
-  QString file = QFileDialog::getSaveFileName(this, "Enregistrer une scene", QString(), "Images *.png *.gif *.jpeg");
-  _image->save(file, "PNG");
+  QString file = QFileDialog::getSaveFileName(this, "Enregistrer une scene", QString(), "Images *.png *.gif *.jpeg *.bmp");
+  format = file.toStdString();
+  format = format.substr(format.size() - 3, format.size());
+  if (format.compare(validFormat[0]) != 0 &&
+      format.compare(validFormat[1]) != 0 &&
+      format.compare(validFormat[2]) != 0 &&
+      format.compare(validFormat[3]) != 0)
+    {
+      format = "png";
+      file += ".png";
+    }
+  if (format.compare("peg") == 0)
+    format = "jpeg";
+  cout << format <<  endl << file.toStdString() << endl;
+  _image->save(file, format.c_str());
 }
 
 void	Gui::putPixel(const Color& color, int x, int y)
