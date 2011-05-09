0c5920cde89cb77ed337704815a183071444d377
franck.lavisse <franck.lavisse@epitech.eu>
2011-05-09 15:18:30 +0000

    ajout d'une toolbar qui poutre

--- src/gui/guiMenuBar.cpp
+++ src/gui/guiMenuBar.cpp
@@ -5,13 +5,14 @@
 // Login   <laviss_f@epitech.net>
 // 
 // Started on  Fri Apr 29 19:30:28 2011 franck lavisse
-// Last update Mon May  9 17:03:51 2011 franck lavisse
+// Last update Mon May  9 17:17:16 2011 franck lavisse
 //
 #include <QMenuBar>
 #include <QAction>
 #include <QHBoxLayout>
 #include <QString>
 #include <iostream>
+#include <QToolBar>
 #include <QKeySequence>
 #include "gui.hpp"
 
@@ -26,6 +27,7 @@ void	Gui::menuBar(void)
   QAction	*saveConfig = new QAction("&Sauvegarder la configuration de la scene", this);
   QAction	*newScene = new QAction("&Nouvelle scene", this);
 
+  _toolbar = addToolBar("42");
   QObject::connect(quitter, SIGNAL(triggered()), qApp, SLOT(quit()));
   file->addAction(newScene);
   file->addAction(openScene);
@@ -37,11 +39,8 @@ void	Gui::menuBar(void)
   newScene->setIcon(QIcon("nouveau.png"));
   saveConfig->setIcon(QIcon("save.png"));
   setMenuBar(menu);
-  //_DockBar->setAllowedAreas(Qt::TopDockWidgetArea);
-  //  _DockBar->setFeatures(0);  
-  //_DockBar->setTitleBarWidget(0);
-  //box->addWidget(menu);
-  //_widgetBar->setLayout(box);
-  //_DockBar->setWidget(_widgetBar);
-  //addDockWidget(Qt::TopDockWidgetArea, _DockBar);
+  _toolbar->addAction(newScene);
+  _toolbar->addAction(openScene);
+  _toolbar->addAction(saveConfig);
+  _toolbar->addAction(quitter);
 }
