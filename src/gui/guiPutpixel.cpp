ac4233e3adfb29aa53cc2a8a28805a8b61f39ee2
franck.lavisse <franck.lavisse@epitech.eu>
2011-05-10 11:10:48 +0000

    export done

--- src/gui/guiPutpixel.cpp
+++ src/gui/guiPutpixel.cpp
@@ -5,13 +5,14 @@
 // Login   <laviss_f@epitech.net>
 // 
 // Started on  Fri Apr 29 15:39:43 2011 franck lavisse
-// Last update Tue May 10 12:22:00 2011 loick michard
+// Last update Tue May 10 13:10:23 2011 franck lavisse
 //
 #include <iostream>
 #include <string>
 #include <vector>
 #include <QDialog>
 #include <QFileDialog>
+#include <stdlib.h>
 #include "gui.hpp"
 #include "../Color.hpp"
 
@@ -19,13 +20,21 @@ using namespace std;
 
 void	Gui::saveImage(void)
 {
-  //  _image->save("raytracer.png", "PNG");
-  //QString file = QFileDialog::getExistingDirectory(this);
-  QString file = QFileDialog::getSaveFileName(this, "Enregistrer une scene", QString(), "Images (*.png *.gif *.jpeg *.bmp)");
-  /*if (file != NULL)
+  int	found;
+  vector<string> validFormat(4);
+  validFormat[0] = "png";
+  validFormat[1] = "gif";
+  validFormat[2] = "bmp";
+  validFormat[3] = "jpeg";
+  string format;
+  QString file = QFileDialog::getSaveFileName(this, "Enregistrer une scene", QString(), "*.png;;*.gif;;*.jpeg;;*.bmp", 0, QFileDialog::DontUseNativeDialog);
+
+  if (file != NULL)
     {
       format = file.toStdString();
-      format = format.substr(format.size() - 3, format.size());
+      found = format.find(".");
+      format = format.substr(found + 1);
+      cout << format << endl;
       if (format.compare(validFormat[0]) != 0 &&
 	  format.compare(validFormat[1]) != 0 &&
 	  format.compare(validFormat[2]) != 0 &&
@@ -34,10 +43,8 @@ void	Gui::saveImage(void)
 	  format = "png";
 	  file += ".png";
 	}
-      if (format.compare("peg") == 0)
-	format = "jpeg";
       cout << format <<  endl << file.toStdString() << endl;
-    }*/
+    }
   _image->save(file);
 }
 
