70cf33c0f5aca862d39a82a182bd7654cb321d2e
melvin.laplanche@gmail.com <melvin.laplanche@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-12 12:13:32 +0000

    add missing files

---
+++ trunk/src/SceneLight.cpp
@@ -0,0 +1,341 @@
+//
+// SceneLight.cpp for  in /home/laplan_m/Src/Epitech/Projects/C++/raytracer-42
+//
+// Made by melvin laplanche
+// Login   <laplan_m@epitech.net>
+//
+// Started on  Wed May 11 17:09:42 2011 melvin laplanche
+// Last update Thu May 12 00:37:26 2011 melvin laplanche
+//
+
+#include "Scene.hpp"
+
+Spot*			Scene::_parseSpotLight(QDomNode	n)
+{
+  Spot*			light = new Spot();
+  bool			position = false;
+  bool			color = false;
+  bool			intensity = false;
+  bool			directLight = false;
+
+  while (n.isNull() == false)
+  {
+    if (n.hasChildNodes() == false || n.isElement() == false)
+      this->_putError("Every light children must be an element", n);
+    if (n.nodeName() == "position")
+    {
+      if (position)
+	this->_putWarning("A light has several position, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "position"));
+	position = true;
+      }
+    }
+    else if (n.nodeName() == "color")
+    {
+      if (color)
+	this->_putWarning("A light has several color, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setColor(_parseColor(n));
+	color = true;
+      }
+    }
+    else if (n.nodeName() == "intensity")
+    {
+      if (intensity)
+	this->_putWarning("A light has several intensity, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setIntensity(_parseDouble(n, 0, 0, "intensity"));
+	intensity = true;
+      }
+    }
+    else if (n.nodeName() == "directLight")
+    {
+      if (directLight)
+	this->_putWarning("A light has several directLight, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setDirectLightPow(_parseDouble(n, 0, 0, "directLight"));
+	directLight = true;
+      }
+    }
+    else
+      this->_putError(n.nodeName().toStdString() + " is not a valid element",
+		      n);
+    n = n.nextSibling();
+  }
+  if (!position || !color || !intensity)
+    this->_putError("A spot must have a position, a color, "
+		    "and an intensity", n);
+  return light;
+}
+
+ParallelLight*			Scene::_parseParallelLight(QDomNode	n)
+{
+  ParallelLight*	light = new ParallelLight();
+  bool			direction = false;
+  bool			color = false;
+  bool			intensity = false;
+
+  while (n.isNull() == false)
+  {
+    if (n.hasChildNodes() == false || n.isElement() == false)
+      this->_putError("Every light children must be an element", n);
+    if (n.nodeName() == "direction")
+    {
+      if (direction)
+	this->_putWarning("A light has several direction, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "direction"));
+	direction = true;
+      }
+    }
+    else if (n.nodeName() == "color")
+    {
+      if (color)
+	this->_putWarning("A light has several colors, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setColor(_parseColor(n));
+	color = true;
+      }
+    }
+    else if (n.nodeName() == "intensity")
+    {
+      if (intensity)
+	this->_putWarning("A light has several intensity, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setIntensity(_parseDouble(n, 0, 0, "intensity"));
+	intensity = true;
+      }
+    }
+    else
+      this->_putError(n.nodeName().toStdString() + " is not a valid element",
+		      n);
+    n = n.nextSibling();
+  }
+  if (!direction || !color || !intensity)
+    this->_putError("A parallel light must have a direction, color, "
+		    "and an intensity", n);
+  return light;
+}
+
+SphericalLight*			Scene::_parseSphericalLight(QDomNode	n)
+{
+  SphericalLight*	light = new SphericalLight();
+  bool			position = false;
+  bool			color = false;
+  bool			intensity = false;
+  bool			size = false;
+  bool			directLight = false;
+
+  while (n.isNull() == false)
+  {
+    if (n.hasChildNodes() == false || n.isElement() == false)
+      this->_putError("Every light children must be an element", n);
+    if (n.nodeName() == "position")
+    {
+      if (position)
+	this->_putWarning("A light has several position, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "position"));
+	position = true;
+      }
+    }
+    else if (n.nodeName() == "color")
+    {
+      if (color)
+	this->_putWarning("A light has several colors, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setColor(_parseColor(n));
+	color = true;
+      }
+    }
+    else if (n.nodeName() == "intensity")
+    {
+      if (intensity)
+	this->_putWarning("A light has several intensity, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setIntensity(_parseDouble(n, 0, 0, "intensity"));
+	intensity = true;
+      }
+    }
+    else if (n.nodeName() == "size")
+    {
+      if (size)
+	this->_putWarning("A light has several size, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setSize(_parseDouble(n, 0, 0, "size"));
+	size = true;
+      }
+    }
+    else if (n.nodeName() == "directLight")
+    {
+      if (directLight)
+	this->_putWarning("A light has several directLight, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setDirectLightPow(_parseDouble(n, 0, 0, "directLight"));
+	directLight = true;
+      }
+    }
+    else
+      this->_putError(n.nodeName().toStdString() + " is not a valid element",
+		      n);
+    n = n.nextSibling();
+  }
+  if (!position || !color || !intensity || !size)
+    this->_putError("A spherical light must have a position, a color, a size, "
+		    "and an intensity", n);
+  return light;
+}
+
+ParallelogramLight*		Scene::_parseParallelogramLight(QDomNode n)
+{
+  ParallelogramLight*	light = new ParallelogramLight();
+  bool			p1 = false;
+  bool			p2 = false;
+  bool			p3 = false;
+  bool			color = false;
+  bool			intensity = false;
+  bool			directLight = false;
+
+  while (n.isNull() == false)
+  {
+    if (n.hasChildNodes() == false || n.isElement() == false)
+      this->_putError("Every light children must be an element", n);
+    if (n.nodeName() == "point1")
+    {
+      if (p1)
+	this->_putWarning("A light has several point1, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "point1"));
+	p1 = true;
+      }
+    }
+    else if (n.nodeName() == "point2")
+    {
+      if (p2)
+	this->_putWarning("A light has several point2, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "point2"));
+	p2 = true;
+      }
+    }
+    else if (n.nodeName() == "point3")
+    {
+      if (p3)
+	this->_putWarning("A light has several point3, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setPosition(_parsePosition(n, "point3"));
+	p3 = true;
+      }
+    }
+    else if (n.nodeName() == "color")
+    {
+      if (color)
+	this->_putWarning("A light has several colors, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setColor(_parseColor(n));
+	color = true;
+      }
+    }
+    else if (n.nodeName() == "intensity")
+    {
+      if (intensity)
+	this->_putWarning("A light has several intensity, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setIntensity(_parseDouble(n, 0, 0, "intensity"));
+	intensity = true;
+      }
+    }
+    else if (n.nodeName() == "directLight")
+    {
+      if (directLight)
+	this->_putWarning("A light has several directLight, "
+			  "the first defined will be used", n);
+      else
+      {
+	light->setDirectLightPow(_parseDouble(n, 0, 0, "directLight"));
+	directLight = true;
+      }
+    }
+    else
+      this->_putError(n.nodeName().toStdString() + " is not a valid element",
+		      n);
+    n = n.nextSibling();
+  }
+  if (!p1 || !p2 || !p3 || !color || !intensity)
+    this->_putError("A parallelogram light must have a point1, a point2, "
+		    "a point3, a color, and an intensity", n);
+  return light;
+}
+
+void			Scene::_parseLight(QDomNode n)
+{
+  QDomNamedNodeMap	nodeMap;
+  QString		type;
+
+  while (n.isNull() == false)
+  {
+    if (n.nodeName() != "light" || n.isElement() == false)
+      this->_putError("A lights child cannot be empty and must be a "
+		      "light element", n);
+    if (n.hasChildNodes() == false)
+      this->_putError("A light element cannot be empty", n);
+    if (n.hasAttributes() != false && n.attributes().contains("type"))
+      type = n.attributes().namedItem("type").nodeValue();
+    if ((n.hasAttributes() == false
+	 || n.attributes().contains("type") == false))
+      this->_putError(type.toStdString() + " is not a valide light type", n);
+    else if (type == "spot")
+      this->_lights.push_back(this->_parseSpotLight(n.firstChild()));
+    else if (type == "parallel")
+      this->_lights.push_back(this->_parseParallelLight(n.firstChild()));
+    else if (type == "spherical")
+      this->_lights.push_back(this->_parseSphericalLight(n.firstChild()));
+    else if (type == "parallelogram")
+      this->_lights.push_back(this->_parseParallelogramLight(n.firstChild()));
+    else
+      this->_putError(type.toStdString() + " is not a valide light type", n);
+    n = n.nextSibling();
+  }
+}
+
+void			Scene::_parseLights(QDomNode n)
+{
+  if (n.hasChildNodes() == false)
+    this->_putError("A lights element cannot be empty", n);
+  this->_parseLight(n.firstChild());
+}
---
+++ trunk/src/SceneMaterial.cpp
@@ -0,0 +1,267 @@
+//
+// SceneMaterial.cpp for  in /home/laplan_m/Src/Epitech/Projects/C++/raytracer-42
+//
+// Made by melvin laplanche
+// Login   <laplan_m@epitech.net>
+//
+// Started on  Wed May 11 16:59:26 2011 melvin laplanche
+// Last update Wed May 11 21:37:25 2011 melvin laplanche
+//
+
+#include "Scene.hpp"
+
+
+void			Scene::_failIfMaterialNameExists(QString	name,
+							 QDomNode	n)
+{
+  int		nbMat = this->_materials.size();
+  string	toFind = name.toStdString();
+
+  for (int i = 0; i < nbMat; i++)
+    if (this->_materials[i]->getName() == toFind)
+      this->_putError("The material " + toFind + " already exists", n);
+}
+
+void			Scene::_failIfMaterialNameDoesntExists(QString	name,
+							       QDomNode	n)
+{
+  int		nbMat = this->_materials.size();
+  string	toFind = name.toStdString();
+
+  for (int i = 0; i < nbMat; i++)
+  {
+    if (this->_materials[i]->getName() == toFind)
+      return ;
+  }
+  this->_putError("The material " + toFind + " doesnt exists"
+		  " (you must define a material before use it)", n);
+}
+
+
+Material		Scene::_getMaterialByName(QString	name)
+{
+  int		nbMat = this->_materials.size();
+  string	toFind = name.toStdString();
+
+  for (int i = 0; i < nbMat; i++)
+    if (this->_materials[i]->getName() == toFind)
+      return *_materials[i];
+}
+
+void			Scene::_parseMaterialOptions(QDomNode	n,
+						     QString	name)
+{
+  Material	*mat = new Material(name.toStdString());
+  bool		color = false;
+  bool		texture = false;
+  bool		limitTexture = false;
+  bool		specular = false;
+  bool		specularPow = false;
+  bool		reflection = false;
+  bool		transmission = false;
+  bool		refractionIndex = false;
+  bool		heightmap = false;
+  bool		normalDef = false;
+  bool		diffusedRef = false;
+
+  while (n.isNull() == false)
+  {
+    if (n.hasChildNodes() == false || n.isElement() == false)
+      this->_putError("Every material children must be an element", n);
+    if (n.nodeName() == "color")
+    {
+      if (color)
+	this->_putWarning("A material has several colors, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setColor(this->_parseColor(n));
+	color = true;
+      }
+    }
+    else if (n.nodeName() == "heightmap")
+    {
+      if (heightmap)
+	this->_putWarning("A material has several heightmaps, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setHeightmap(this->_parseTexture(n, "heightmap"));
+	heightmap = true;
+      }
+    }
+    else if (n.nodeName() == "texture")
+    {
+      if (texture)
+	this->_putWarning("A material has several textures, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setTexture(this->_parseTexture(n, "texture"));
+	texture = true;
+      }
+    }
+    else if (n.nodeName() == "limitTexture")
+    {
+      if (limitTexture)
+	this->_putWarning("A material has several limitTextures, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setLimitTexture(this->_parseTexture(n, "limitTexture"));
+	limitTexture = true;
+      }
+    }
+    else if (n.nodeName() == "specular")
+    {
+      if (specular)
+	this->_putWarning("A material has several speculars, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setSpecularCoeff(this->_parseDouble(n, 0.d, 0.d, "specular"));
+	specular = true;
+      }
+    }
+    else if (n.nodeName() == "specularPow")
+    {
+      if (specularPow)
+	this->_putWarning("A material has several specularPows, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setSpecularPow(this->_parseDouble(n, 0.d, 0.d, "specularPow"));
+	specularPow = true;
+      }
+    }
+    else if (n.nodeName() == "reflection")
+    {
+      if (reflection)
+	this->_putWarning("A material has several reflections, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setReflectionCoeff(this->_parseDouble(n, 0.d, 0.d, "reflection"));
+	reflection = true;
+      }
+    }
+    else if (n.nodeName() == "transmission")
+    {
+      if (transmission)
+	this->_putWarning("A material has several transmissions, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setTransmissionCoeff(_parseDouble(n, 0.d, 0.d, "transmission"));
+	transmission = true;
+      }
+    }
+    else if (n.nodeName() == "refractionIndex")
+    {
+      if (refractionIndex)
+	this->_putWarning("A material has several refractionIndexs, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setRefractionIndex(_parseDouble(n, 0.d, 0.d, "refractionIndex"));
+	refractionIndex = true;
+      }
+    }
+    else if (n.nodeName() == "diffusedReflection")
+    {
+      if (diffusedRef)
+	this->_putWarning("A material has several diffusedReflections, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setDiffusedReflectionCoeff(this->_parseInt(n, 0.d, 0.d,
+						  "diffusedReflection"));
+	diffusedRef = true;
+      }
+    }
+    else if (n.nodeName() == "diffusedReflection")
+    {
+      if (diffusedRef)
+	this->_putWarning("A material has several diffusedReflections, "
+			  "the first defined will be used", n);
+      else
+      {
+	mat->setDiffusedReflectionCoeff(this->_parseInt(n, 0.d, 0.d,
+							"diffusedReflection"));
+	diffusedRef = true;
+      }
+    }
+    else if (n.nodeName() == "normalDeformation")
+    {
+      if (normalDef)
+	this->_putWarning("A material has several normalDeformations, "
+			  "the first defined will be used", n);
+      else
+      {
+	this->_parseNormalDef(n, mat);
+	normalDef = true;
+      }
+    }
+    else
+      this->_putError(n.nodeName().toStdString() + " is not a valid element",
+		      n);
+    n = n.nextSibling();
+  }
+  this->_materials.push_back(mat);
+}
+
+void			Scene::_parseMaterial(QDomNode n)
+{
+  QDomNamedNodeMap	nodeMap;
+  QString		name;
+
+  while (n.isNull() == false)
+  {
+    if (n.nodeName() != "material" || n.isElement() == false)
+      this->_putError("A materials child cannot be empty and must be a "
+			"material element", n);
+    if (n.hasChildNodes() == false)
+      this->_putError("A material element cannot be empty", n);
+    if (n.hasAttributes() == false|| n.attributes().contains("name") == false)
+      this->_putError("The material attributes are missing", n);
+    name = n.attributes().namedItem("name").nodeValue();
+    this->_failIfMaterialNameExists(name, n);
+    this->_parseMaterialOptions(n.firstChild(), name);
+    n = n.nextSibling();
+  }
+}
+
+void			Scene::_parseMaterials(QDomNode n)
+{
+  if (n.hasChildNodes() == false)
+    this->_putError("A materials element cannot be empty", n);
+  this->_parseMaterial(n.firstChild());
+}
+
+
+Texture*			Scene::_parseTexture(QDomNode	n,
+						     string	obj_name)
+{
+  PerlinNoise		*perlin = new PerlinNoise();
+  QString		value;
+  QString		attrValue;
+
+  if (n.hasAttributes() == false || n.attributes().contains("type") == false)
+    this->_putError(obj_name + " type not specified", n);
+  this->_checkContentIsSingleText(n, obj_name);
+  attrValue = n.attributes().namedItem("type").nodeValue();
+  value = n.toElement().text();
+  if (attrValue == "procedural")
+  {
+    if (value == "marble")
+      perlin->setMarbleProperties();
+    else if (value == "wood")
+      perlin->setWoodProperties();
+    else if (value != "perlin")
+      this->_putError(value.toStdString() + " is not a valid texture", n);
+    return perlin;
+  }
+  else if (attrValue == "file")
+    return new Texture(this->_parseFile(n, obj_name));
+  this->_putError("normalDeformation type must be an integer", n);
+}
