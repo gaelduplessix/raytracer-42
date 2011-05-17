b4aa2d7c84fbe1d288f974739ceb7ca34d5a801a
samuel.olivier@epitech.eu <samuel.olivier@epitech.eu@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-17 16:26:47 +0000

    Le photonMapping avance...

--- trunk/src/PhotonMap.hpp
+++ trunk/src/PhotonMap.hpp
@@ -5,7 +5,7 @@
 // Login   <olivie_a@epitech.net>
 // 
 // Started on  Tue May 17 13:36:45 2011 samuel olivier
-// Last update Tue May 17 14:44:24 2011 samuel olivier
+// Last update Tue May 17 18:24:12 2011 samuel olivier
 //
 
 #ifndef _PHOTON_MAP_
@@ -17,12 +17,17 @@
 
 using namespace std;
 
+class Raytracer;
+
 class PhotonMap
 {
 public:
   PhotonMap(const vector<Photon>& map);
   const vector<Photon>&	getMap(void);
   void			setMap(const vector<Photon>& map);
+  void			throwRay(Raytracer *rt, const Point& position,
+				 int sampling, const Color& currentColor);
+  void			fillPhotonMap(Raytracer *rt);
 
 private:
   vector<Photon> _map;
