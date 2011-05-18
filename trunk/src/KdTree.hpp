449b234a4966b07b4f45e7c708fd28701e250630
loick.michard@gmail.com <loick.michard@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-18 08:29:32 +0000

    KdTree done (humour)

---
+++ trunk/src/KdTree.hpp
@@ -0,0 +1,20 @@
+//
+// KdTree.hpp for raytracer in /home/michar_l//Raytracer/raytracer-42
+// 
+// Made by loick michard
+// Login   <michar_l@epitech.net>
+// 
+// Started on  Wed May 18 10:27:25 2011 loick michard
+// Last update Wed May 18 10:28:07 2011 loick michard
+//
+
+#ifndef _KD_TREE_HPP_
+#define _KD_TREE_HPP_
+
+class KdTree
+{
+  KdTree();
+  ~KdTree();
+};
+
+#endif
