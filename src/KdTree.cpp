927afd0a733ffc4f126d434660eadcf04eb73779
loick.michard <loick.michard@gmail.com>
2011-05-19 14:25:27 +0000

    GUI += threads

--- src/KdTree.cpp
+++ src/KdTree.cpp
@@ -5,14 +5,14 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Wed May 18 10:28:17 2011 loick michard
-// Last update Wed May 18 10:28:59 2011 loick michard
+// Last update Wed May 18 14:01:19 2011 loick michard
 //
 
 #include "KdTree.hpp"
 
-KdTree::KdTree()
+KdTree::KdTree(const vector<Object*>& objects)
 {
-
+  
 }
 
 KdTree::~KdTree()
--- src/KdTree.hpp
+++ src/KdTree.hpp
@@ -5,15 +5,27 @@
 // Login   <michar_l@epitech.net>
 // 
 // Started on  Wed May 18 10:27:25 2011 loick michard
-// Last update Wed May 18 10:28:07 2011 loick michard
+// Last update Wed May 18 14:16:41 2011 loick michard
 //
 
 #ifndef _KD_TREE_HPP_
 #define _KD_TREE_HPP_
 
+#include "Object.hpp"
+#include "BoundingBox.hpp"
+
+typedef struct	kdTreeNode
+{
+  vector<ObjectPrimitive*>	primitives;
+  BoundingBox			box;
+  struct kdTreeNode		*leftChild;
+  struct kdTreeNode		*rightChild;
+  struct kdTreeNode		*parent;
+}		kdTreeNode;
+
 class KdTree
 {
-  KdTree();
+  KdTree(const vector<Object*>& objects);
   ~KdTree();
 };
 
