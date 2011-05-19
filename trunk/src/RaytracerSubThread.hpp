033c57cac94a6ad4f9601064482fc3acdcd48189
gael.duplessix@gmail.com <gael.duplessix@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-19 14:21:36 +0000

    avc les fichiers c'est mieux

---
+++ trunk/src/RaytracerSubThread.hpp
@@ -0,0 +1,33 @@
+//
+// RaytracerSubThread.hpp for raytracer in /home/jochau_g//Desktop/raytracer-42/src
+// 
+// Made by gael jochaud-du-plessix
+// Login   <jochau_g@epitech.net>
+// 
+// Started on  Thu May 19 15:21:07 2011 gael jochaud-du-plessix
+// Last update Thu May 19 16:16:08 2011 gael jochaud-du-plessix
+//
+
+#ifndef _RAYTRACERSUBTHREAD_HPP_
+#define _RAYTRACERSUBTHREAD_HPP_
+
+#include <QThread>
+
+class RaytracerThread;
+
+class	RaytracerSubThread : public QThread
+{
+public:
+  RaytracerSubThread(RaytracerThread* mainThread, double startProgress,
+		     double endProgress);
+
+  void	run(void);
+
+private:
+  RaytracerThread*	_mainThread;
+  double		_startProgress;
+  double		_endProgress;
+  double		_currentProgress;
+};
+
+#endif
