a0692652506c8bdd86941945db1cd1dc85b3e551
gael.duplessix@gmail.com <gael.duplessix@gmail.com@fa825092-d137-ea9c-04ad-3f3e2b08b733>
2011-05-23 12:32:04 +0000

    Creation des classes server et client

---
+++ trunk/src/ClusterClient.hpp
@@ -0,0 +1,24 @@
+//
+// ClusterClient.hpp for raytracer in /home/jochau_g//Desktop/raytracer-42/src
+// 
+// Made by gael jochaud-du-plessix
+// Login   <jochau_g@epitech.net>
+// 
+// Started on  Mon May 23 13:12:10 2011 gael jochaud-du-plessix
+// Last update Mon May 23 13:15:13 2011 gael jochaud-du-plessix
+//
+
+#include <string>
+
+using namespace std;
+
+#ifndef _CLUSTERCLIENT_HPP_
+#define _CLUSTERCLIENT_HPP_
+
+class ClusterClient
+{
+public:
+  ClusterClient(string url);
+};
+
+#endif
